24f8bf45dfcc03deb2e51c576863f64b6e386934
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2025-02-27 22:31:28 +0100

    Update app.js

--- src/projects/budget/app.js
+++ src/projects/budget/app.js
@@ -1,36 +1,85 @@
 //Variables y selectores
 const listExpense = document.querySelector("#expense ul");
 const inputBudget = document.getElementById("budgetInput");
-const formExpense = document.getElementById("budgetForm");
-
-const budgetValue = [];
-
+const budgetForm = document.getElementById("budgetForm");
+const spanAmount = document.getElementById("amount");
+const spanTotal = document.getElementById("total");
+const spanRemaining = document.getElementById("reamining");
+const formExpense = document.getElementById("addExpenseForm");
+let budget;
 //Eventos
-formExpense.addEventListener("submit", (event) => {
+budgetForm.addEventListener("submit", (event) => {
   event.preventDefault(); // Evita recargar la página
   loadBudget();
+});
+
+formExpense.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addExpense();
 });
 //Clases
 class Budget {
   constructor(budgetProp) {
-    this.budget = budgetProp;
-    this.sub = budgetProp;
+    this.amount = Number(budgetProp);
+    this.subAmount = Number(budgetProp);
     this.expense = [];
+  }
+ 
+  calculateSubAmount() {
+    const expensed = this.expense.reduce(
+      (total, curr) => total + curr.qtyExpense,
+      0
+    );
+    this.subAmount = Number(this.amount - expensed);
+  }
+  insertBudget() {
+    document.getElementById("total").innerText = this.amount;
+    document.getElementById("remaining").innerText = this.subAmount;
+  }
+  calculateExpense(propExpense) {
+    this.expense = [...this.expense, propExpense];
+    this.calculateSubAmount();
+    this.insertBudget();
+    console.log(this.expense);
+    
   }
 }
 
-class UserInterface {}
-
+class UserInterface {
+  constructor(uiProp) {}
+  insertAlert(message, type, id) {
+    const background = type === "error" ? "bg-red-500" : "bg-green-400";
+    const alertBox = document.createElement("p");
+    alertBox.innerHTML = `<p id=${id} class='text-center ${background}'>${message}</p>`;
+    budgetForm.appendChild(alertBox);
+  }
+  rmvElement(id) {
+    setTimeout(() => {
+      document.getElementById(id).remove();
+    }, 2000);
+  }
+}
+//Instancias
+const ui = new UserInterface();
+budget = new Budget();
 //Funciones
+function addExpense() {
+  const nameExp = document.getElementById("nameExpense").value;
+  const qtyExpense = Number(document.getElementById("quantityExpense").value);
+  const expense = { nameExp, qtyExpense, id: Date.now() };
+  budget.calculateExpense(expense);
+}
 //Carga de presupuesto
 const loadBudget = () => {
   const valor = parseFloat(inputBudget.value);
   if (!isNaN(valor) && valor > 0) {
-    budgetValue[0] = valor;
+    budget = new Budget(valor);
     inputBudget.disabled = true;
-    console.log(budgetValue);
+    spanAmount.innerText = valor.toLocaleString();
+    budget.insertBudget();
   } else {
-    alert("Ingrese un número válido.");
+    ui.insertAlert("Ingrese un número válido.", "error", "errorEntry");
+    ui.rmvElement("errorEntry");
   }
 };
 const isDisabledBudgetButton = () => {
@@ -38,3 +87,5 @@ const isDisabledBudgetButton = () => {
   inputBudget.value = "";
   return;
 };
+
+//agregar gastos
